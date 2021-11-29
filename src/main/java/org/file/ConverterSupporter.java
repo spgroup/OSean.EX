2013d26caa878568745da129d612bb38d6f5cd34
leusonmario <leusonmario@gmail.com>
2021-11-29 15:21:42 -0300

    validating all required converters

--- src/main/java/org/file/ConverterSupporter.java
+++ src/main/java/org/file/ConverterSupporter.java
@@ -13,39 +13,58 @@ public class ConverterSupporter extends SerializedObjectAccessOutputClass{
   public boolean getOutputClass(List<String> serializedObjectMethods, String pomFileDirectory, String packageName){
     try {
       for (String oneConverter : serializedObjectMethods) {
-        String classText = ""
-            + "package "+packageName+";\n\n"
-            + "import com.thoughtworks.xstream.converters.Converter;\n"
-            + "import com.thoughtworks.xstream.converters.MarshallingContext;\n"
-            + "import com.thoughtworks.xstream.converters.UnmarshallingContext;\n"
-            + "import com.thoughtworks.xstream.io.HierarchicalStreamReader;\n"
-            + "import com.thoughtworks.xstream.io.HierarchicalStreamWriter;\n"
-            + "import java.util.ArrayList;\n"
-            + "\npublic class Converter"+ getClassName(oneConverter) +" implements Converter {\n"
-            + "  public boolean canConvert(Class clazz) {\n"
-            + "    boolean interfaceImp = "+oneConverter+".class.isAssignableFrom(clazz);\n"
-            + "    boolean sameClass = clazz.equals("+oneConverter+".class);\n"
-            + "    return interfaceImp || sameClass;\n"
-            + "  }\n"
-            + "\n"
-            + "  public void marshal(Object value, HierarchicalStreamWriter writer,\n"
-            + "      MarshallingContext context) {\n"
-            + "  }\n"
-            + "\n"
-            + "  public Object unmarshal(HierarchicalStreamReader reader,\n"
-            + "      UnmarshallingContext context) {\n"
-            + "    return null;\n"
-            + "  }\n"
-            + "\n}";
-        saveFile(pomFileDirectory, classText, "Converter"+getClassName(oneConverter));
-        this.classesPathSignature.add(packageName+".Converter"+getClassName(oneConverter));
-        this.outputClassesPath.add(new File(pomFileDirectory+File.separator+"Converter"+getClassName(oneConverter)+".java").toPath());
+        if (isFileOnProject(oneConverter, new File(pomFileDirectory))) {
+          String classText = ""
+              + "package " + packageName + ";\n\n"
+              + "import com.thoughtworks.xstream.converters.Converter;\n"
+              + "import com.thoughtworks.xstream.converters.MarshallingContext;\n"
+              + "import com.thoughtworks.xstream.converters.UnmarshallingContext;\n"
+              + "import com.thoughtworks.xstream.io.HierarchicalStreamReader;\n"
+              + "import com.thoughtworks.xstream.io.HierarchicalStreamWriter;\n"
+              + "import java.util.ArrayList;\n"
+              + "\npublic class Converter" + getClassName(oneConverter)
+              + " implements Converter {\n"
+              + "  public boolean canConvert(Class clazz) {\n"
+              + "    boolean interfaceImp = " + oneConverter + ".class.isAssignableFrom(clazz);\n"
+              + "    boolean sameClass = clazz.equals(" + oneConverter + ".class);\n"
+              + "    return interfaceImp || sameClass;\n"
+              + "  }\n"
+              + "\n"
+              + "  public void marshal(Object value, HierarchicalStreamWriter writer,\n"
+              + "      MarshallingContext context) {\n"
+              + "  }\n"
+              + "\n"
+              + "  public Object unmarshal(HierarchicalStreamReader reader,\n"
+              + "      UnmarshallingContext context) {\n"
+              + "    return null;\n"
+              + "  }\n"
+              + "\n}";
+          saveFile(pomFileDirectory, classText, "Converter" + getClassName(oneConverter));
+          this.classesPathSignature.add(packageName + ".Converter" + getClassName(oneConverter));
+          this.outputClassesPath.add(new File(
+              pomFileDirectory + File.separator + "Converter" + getClassName(oneConverter)
+                  + ".java").toPath());
+        }
 
       }
       return true;
     }catch (Exception e){
       e.printStackTrace();
     }
+    return false;
+  }
+
+  public boolean isFileOnProject(String fileName, File file) {
+    fileName = fileName.substring(fileName.lastIndexOf(".")+1)+".java";
+    File[] list = file.listFiles();
+    if(list!=null)
+      for (File fil : list){
+        if (fil.isDirectory()){
+          isFileOnProject(fileName, fil);
+        }else if (fileName.equalsIgnoreCase(fil.getName())) {
+          return true;
+        }
+      }
     return false;
   }
 
