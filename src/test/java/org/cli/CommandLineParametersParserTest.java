b006ecddc3e0628c15271768aa199f59f27dd34b
Joao Pedro Henrique <joao.pedro.hsd@gmail.com>
2022-05-19 19:20:08 -0300

    tests: add more tests

--- src/test/java/org/cli/CommandLineParametersParserTest.java
+++ src/test/java/org/cli/CommandLineParametersParserTest.java
@@ -1,6 +1,8 @@
 package org.cli;
 
 import java.io.File;
+import java.util.Arrays;
+import java.util.List;
 
 import org.junit.Assert;
 import org.junit.jupiter.api.Test;
@@ -55,5 +57,93 @@ class CommandLineParametersParserTest {
     MergeScenarioUnderAnalysis result = getCommandLineParametersParser().parse(args);
 
     Assert.assertEquals("gradle", result.getBuildManager());
+  }
+
+  @Test
+  void mergeScenarioCommitsAreProvidedAsCommaSeparated() {
+    String[] args = {
+        "-l",
+        TOY_PROJECT_LOCATION,
+        "-c",
+        "Person",
+        "-m",
+        "getName",
+        "-p",
+        "toy-project",
+        "-h",
+        "7810b85dd711ac2648675dcfe5e65539aec1ea1d,85077377978f98e31e637c121b5987e01725f5fd"
+    };
+
+    MergeScenarioUnderAnalysis result = getCommandLineParametersParser().parse(args);
+    List<String> expectedCommitHashes = Arrays.asList(
+        "7810b85dd711ac2648675dcfe5e65539aec1ea1d",
+        "85077377978f98e31e637c121b5987e01725f5fd");
+
+    Assert.assertEquals(expectedCommitHashes, result.getMergeScenarioCommits());
+  }
+
+  @Test
+  void transformationsAreDisabledByDefault() {
+    String[] args = {
+        "-l",
+        TOY_PROJECT_LOCATION,
+        "-c",
+        "Person",
+        "-m",
+        "getName",
+        "-p",
+        "toy-project",
+        "-h",
+        "7810b85dd711ac2648675dcfe5e65539aec1ea1d"
+    };
+
+    MergeScenarioUnderAnalysis result = getCommandLineParametersParser().parse(args);
+
+    Assert.assertFalse(result.getTransformationOption().applyTransformations());
+    Assert.assertFalse(result.getTransformationOption().applyFullTransformations());
+  }
+
+  @Test
+  void transformationsHaveADefaultBudgetOf60() {
+    String[] args = {
+        "-l",
+        TOY_PROJECT_LOCATION,
+        "-c",
+        "Person",
+        "-m",
+        "getName",
+        "-p",
+        "toy-project",
+        "-att",
+        "-h",
+        "7810b85dd711ac2648675dcfe5e65539aec1ea1d"
+    };
+
+    MergeScenarioUnderAnalysis result = getCommandLineParametersParser().parse(args);
+
+    Assert.assertTrue(result.getTransformationOption().applyTransformations());
+    Assert.assertEquals(60, result.getTransformationOption().getBudget());
+  }
+
+  @Test
+  void fullTransformationsAreDisabledByDefaultEvenIfTransformationsAreActive() {
+    String[] args = {
+        "-l",
+        TOY_PROJECT_LOCATION,
+        "-c",
+        "Person",
+        "-m",
+        "getName",
+        "-p",
+        "toy-project",
+        "-att",
+        "-h",
+        "7810b85dd711ac2648675dcfe5e65539aec1ea1d"
+    };
+
+    MergeScenarioUnderAnalysis result = getCommandLineParametersParser().parse(args);
+
+    Assert.assertTrue(result.getTransformationOption().applyTransformations());
+    Assert.assertFalse(result.getTransformationOption().applyFullTransformations());
   }
 }
